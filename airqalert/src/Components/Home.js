dd7459ba984a11e0a9350163553dcb15685db435
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-04-15 16:54:39 +0530

    layout

--- airqalert/src/Components/Home.js
+++ airqalert/src/Components/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './HomePage.css';
-import myImage from './Images/Home.jpg'
+import myImage from './Images/Home.png'
 function Home() {
   return (
     <div className="fullscreen-image-container">
       <div className="fullscreen-image">
-        <img src={myImage} alt="Description of the image" />
+        <img src={myImage} alt="home image" />
       </div>
     </div>
   )
